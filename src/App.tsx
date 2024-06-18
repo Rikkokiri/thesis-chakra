8896b33d2f5a9e3141bce4bfa0e2e207a3a81b7c
Ossian Rajala <ossian.rajala@gmail.com>
2024-06-18 23:16:45 +0300

    feat: remove own themeDetector, use Chakra's theme detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Toolbar } from "./components/Toolbar/Toolbar";
-import { useDetectTheme } from "@hooks/useDetectTheme";
-import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { AppFooter } from "./layout/footer";
 import { useDocumentTitle } from "./hooks/useDocumentTitle";
 
 function App() {
-  const { prefersDarkMode, setActiveTheme } = useDetectTheme();
   const { t } = useTranslation();
-
-  useEffect(() => {
-    setActiveTheme(prefersDarkMode);
-  }, [prefersDarkMode, setActiveTheme]);
 
   useDocumentTitle(t("pageTitle.default", { library: t("libraryName") }));
 
