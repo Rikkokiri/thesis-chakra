1f1161563a1843f16217f12f66ad74f10e0f39e5
Ossian Rajala <ossian.rajala@gmail.com>
2024-06-18 21:07:36 +0300

    feat: selectable radio group styled with Chakra components

--- src/components/RadioRange/RadioRange.tsx
+++ src/components/RadioRange/RadioRange.tsx
@@ -1,45 +1,50 @@
-import "./RadioRange.css";
+import { Radio, RadioGroup } from "@chakra-ui/react";
 
 interface RadioOption {
   value: number;
   label: string;
-  optionClassName?: string;
+  optionVariant?: string;
 }
 
 interface RadioRangeProps {
   options: RadioOption[];
-  value: number | undefined;
-  onChange: (value: number) => void;
+  value: string | undefined;
+  onChange: (value: string) => void;
   isReadonly?: boolean;
 }
 
 export const RadioRange = (props: RadioRangeProps) => {
   return (
-    <div className="radio-range">
-      {props.options.map((option, index) => {
-        const isChecked = props.value == option.value;
-        const optionClass = option.optionClassName ?? "";
-
-        return (
-          <div
-            className={`radio-range__option ${optionClass}`}
-            key={`radio-option-${option.value}`}
-          >
-            <input
-              type="radio"
-              name="radio-option"
-              id={`radio-option-${option.value}`}
-              value={option.value}
-              checked={isChecked}
-              onChange={() => props.onChange(option.value)}
-              readOnly={!!props.isReadonly}
-              className={props.isReadonly ? "readonly" : ""}
-            />
-            {isChecked && <div className={`option__indicator`}></div>}
-            <label key={index}>{option.label}</label>
-          </div>
-        );
-      })}
-    </div>
+    <RadioGroup
+      onChange={props.onChange}
+      display="flex"
+      w="100%"
+      maxW="var(--radio-range-max-width)"
+      position="relative"
+      justifyContent="space-between"
+      sx={{
+        "::before": {
+          content: '""',
+          position: "absolute",
+          top: "calc(2rem / 2 - 0.25rem / 2)",
+          width: "calc(100% - 2rem)",
+          height: "0.25rem",
+          backgroundColor: "var(--radio-range-bg)",
+          bg: "radioBg",
+        },
+      }}
+    >
+      {props.options.map((option) => (
+        <Radio
+          key={`radio-option-${option.value}`}
+          type="radio"
+          name="radio-option"
+          value={option.value.toString()}
+          variant={option.optionVariant}
+        >
+          {option.label}
+        </Radio>
+      ))}
+    </RadioGroup>
   );
 };
---
+++ src/theme/components/radio.ts
@@ -0,0 +1,110 @@
+import { radioAnatomy } from "@chakra-ui/anatomy";
+import { createMultiStyleConfigHelpers, keyframes } from "@chakra-ui/react";
+import { brand } from "../colors";
+
+const { definePartsStyle, defineMultiStyleConfig } =
+  createMultiStyleConfigHelpers(radioAnatomy.keys);
+
+const scaleOption = keyframes`
+    0%,
+    100% {
+        transform: scale(1);
+    }
+
+    50% {
+        transform: scale(1.5);
+    }
+`;
+
+const radioBg = {
+  backgroundColor: brand.gray30,
+  _dark: {
+    bg: brand.gray60,
+  },
+};
+
+const baseStyle = definePartsStyle({
+  // define the part you're going to style
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    placeItems: "center start",
+    width: "2rem",
+    textAlign: "center",
+    ":first-of-type": {
+      textAlign: "left",
+    },
+    ":last-of-type": {
+      textAlign: "right",
+    },
+  },
+  label: {
+    // fontSize: "12px", // Font-size had to be applied through sizes instead of baseStyle
+    m: 0,
+    mt: "0.75rem",
+  },
+  control: {
+    height: "2rem",
+    width: "2rem",
+    ...radioBg,
+    border: "none",
+    _hover: {
+      ...radioBg,
+    },
+    _checked: {
+      backgroundColor: brand.gray30,
+      borderWidth: "0.25rem",
+      "::before": {
+        height: "1.5rem",
+        width: "1.5rem",
+        backgroundColor: brand.blueBlack,
+      },
+      animation: `${scaleOption} 0.3s ease 0s 1`,
+      _hover: {
+        ...radioBg,
+      },
+    },
+  },
+});
+
+const agree = definePartsStyle({
+  control: {
+    _checked: {
+      "::before": {
+        backgroundColor: brand.success,
+        _dark: {
+          backgroundColor: brand.greenBright,
+        },
+      },
+    },
+  },
+});
+
+const disagree = definePartsStyle({
+  control: {
+    _checked: {
+      "::before": {
+        backgroundColor: brand.error,
+        _dark: {
+          backgroundColor: brand.liveRed,
+        },
+      },
+    },
+  },
+});
+
+export const radioTheme = defineMultiStyleConfig({
+  baseStyle,
+  variants: {
+    agree,
+    disagree,
+  },
+  sizes: {
+    md: {
+      // "md" is the default size for <Radio />
+      label: {
+        fontSize: "0.75rem",
+      },
+    },
+  },
+});
